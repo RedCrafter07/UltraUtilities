0ce5385f8c716ddb19d90e1de21c0d6248c5d156
RedCrafter07 <gh@r07.dev>
2024-04-05 17:56:07 +0200

    Remove shift & refactor

--- src/main/kotlin/redcrafter07/processed/item/WrenchItem.kt
+++ src/main/kotlin/redcrafter07/processed/item/WrenchItem.kt
@@ -1,25 +1,22 @@
 package redcrafter07.processed.item
 
-import net.minecraft.network.chat.Component
 import net.minecraft.world.InteractionResult
 import net.minecraft.world.item.Item
 import net.minecraft.world.item.ItemStack
 import net.minecraft.world.item.context.UseOnContext
-import redcrafter07.processed.ProcessedMod
 import redcrafter07.processed.block.WrenchInteractableBlock
 
 class WrenchItem : Item(Properties().stacksTo(1)) {
 
     override fun onItemUseFirst(stack: ItemStack, context: UseOnContext): InteractionResult {
-        val blockState = context.level.getBlockState(context.clickedPos) ?: return super.onItemUseFirst(stack, context);
-        val block = blockState.block;
-        val withShift = context.player?.isShiftKeyDown ?: false;
+        val blockState = context.level.getBlockState(context.clickedPos) ?: return super.onItemUseFirst(stack, context)
+        val block = blockState.block
 
         if (block is WrenchInteractableBlock) {
-            block.onWrenchUse(context, blockState, withShift);
-            return InteractionResult.SUCCESS;
+            block.onWrenchUse(context, blockState)
+            return InteractionResult.SUCCESS
         } else {
-            return super.onItemUseFirst(stack, context);
+            return super.onItemUseFirst(stack, context)
         }
     }
 }
