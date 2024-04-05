aa4933600dfaad2249a95b406ebeee97b852cf19
RedCrafter07 <gh@r07.dev>
2024-04-05 20:39:24 +0200

    Reformat blocks

--- src/main/kotlin/redcrafter07/processed/block/ModBlocks.kt
+++ src/main/kotlin/redcrafter07/processed/block/ModBlocks.kt
@@ -2,12 +2,12 @@ package redcrafter07.processed.block
 
 import net.minecraft.world.item.BlockItem
 import net.minecraft.world.item.Item
-import redcrafter07.processed.ProcessedMod
 import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.Blocks
 import net.minecraft.world.level.block.state.BlockBehaviour
 import net.neoforged.neoforge.registries.DeferredBlock
 import net.neoforged.neoforge.registries.DeferredRegister
+import redcrafter07.processed.ProcessedMod
 import redcrafter07.processed.item.ModItems
 import java.util.function.Supplier
 
@@ -16,22 +16,21 @@ object ModBlocks {
 
     // the returned ObjectHolderDelegate can be used as a property delegate
     // this is automatically registered by the deferred registry at the correct times
-    val BLITZ_ORE =
-        registerBlock("blitz_ore") {
-            Block(BlockBehaviour.Properties.ofFullCopy(Blocks.DIAMOND_ORE).explosionResistance(1200f))
-        };
-    val BLOCK_PIPE = registerBlock("pipe") { BlockPipe() };
+    val BLITZ_ORE = registerBlock("blitz_ore") {
+        Block(BlockBehaviour.Properties.ofFullCopy(Blocks.DIAMOND_ORE).explosionResistance(1200f))
+    }
+    val BLOCK_PIPE = registerBlock("pipe") { BlockPipe() }
 
     private fun <T : Block> registerBlock(name: String, block: Supplier<T>): DeferredBlock<T> {
         // this will call registerItem later, that's why it's not implemented yet
-        val regBlock = BLOCKS.register(name, block);
+        val regBlock = BLOCKS.register(name, block)
 
-        registerBlockItem(name, regBlock);
+        registerBlockItem(name, regBlock)
 
-        return regBlock;
+        return regBlock
     }
 
     private fun <T : Block> registerBlockItem(name: String, block: DeferredBlock<T>) {
-        ModItems.registerItem(name) { BlockItem(block.get(), Item.Properties()) };
+        ModItems.registerItem(name) { BlockItem(block.get(), Item.Properties()) }
     }
 }
