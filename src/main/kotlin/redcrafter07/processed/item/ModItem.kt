5c7f58cfa64a2fe227eb4eb5e73ff592193e2e56
RedCrafter07 <gh@r07.dev>
2024-04-05 22:53:22 +0200

    Add processed.

--- src/main/kotlin/redcrafter07/processed/item/ModItem.kt
+++ src/main/kotlin/redcrafter07/processed/item/ModItem.kt
@@ -7,10 +7,12 @@ import net.minecraft.world.item.ItemStack
 import net.minecraft.world.item.TooltipFlag
 import net.minecraft.world.level.Level
 
+
 open class ModItem(properties: Properties, private val itemID: String) : Item(properties) {
     override fun appendHoverText(stack: ItemStack, world: Level?, tooltip: MutableList<Component>, flag: TooltipFlag) {
         if (Screen.hasShiftDown()) {
-            tooltip.add(Component.translatable("item.$itemID.tooltip"))
-        } else tooltip.add(Component.translatable("item.hold_shift"))
+            tooltip.add(Component.translatable("item.processed.$itemID.tooltip"))
+        } else tooltip.add(Component.translatable("item.processed.hold_shift"))
     }
+
 }
