91fa1edece422557db3118f61912e901f5020228
RedCrafter07 <gh@r07.dev>
2024-04-05 20:41:07 +0200

    Remove wrenchInfo logic

--- src/main/kotlin/redcrafter07/processed/item/WrenchItem.kt
+++ src/main/kotlin/redcrafter07/processed/item/WrenchItem.kt
@@ -1,14 +1,12 @@
 package redcrafter07.processed.item
 
-import net.minecraft.core.BlockPos
 import net.minecraft.world.InteractionResult
-import net.minecraft.world.entity.player.Player
-import net.minecraft.world.item.Item
 import net.minecraft.world.item.ItemStack
 import net.minecraft.world.item.context.UseOnContext
 import redcrafter07.processed.block.WrenchInteractableBlock
 
-class WrenchItem : Item(Properties().stacksTo(1)) {
+
+class WrenchItem : ModItem(Properties().stacksTo(1)) {
 
     override fun onItemUseFirst(stack: ItemStack, context: UseOnContext): InteractionResult {
         val blockState = context.level.getBlockState(context.clickedPos) ?: return super.onItemUseFirst(stack, context)
@@ -17,20 +15,13 @@ class WrenchItem : Item(Properties().stacksTo(1)) {
         if (block is WrenchInteractableBlock) {
             block.onWrenchUse(context, blockState)
             return InteractionResult.SUCCESS
-        } else {
-            return super.onItemUseFirst(stack, context)
         }
-    }
-
-    override fun onBlockStartBreak(itemstack: ItemStack, pos: BlockPos, player: Player): Boolean {
-        val block = player.level().getBlockState(pos).block
-
-        if (block is WrenchInteractableBlock) {
-            block.onWrenchInfo(player, player.level().getBlockState(pos))
-
-            return true
-        } else {
-            return super.onBlockStartBreak(itemstack, pos, player)
+        val blockEntity =
+            context.level.getBlockEntity(context.clickedPos) ?: return super.onItemUseFirst(stack, context)
+        if (blockEntity is WrenchInteractableBlock) {
+            blockEntity.onWrenchUse(context, blockState)
+            return InteractionResult.SUCCESS
         }
+        return super.onItemUseFirst(stack, context)
     }
 }
