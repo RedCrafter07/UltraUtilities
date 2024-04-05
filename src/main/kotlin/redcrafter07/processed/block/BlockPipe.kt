81a89f062fc743319c3b8750b6fdfdcbd6b40f35
RedCrafter07 <gh@r07.dev>
2024-04-05 20:55:33 +0200

    Fishi worked on stuff and I push it lol
    Co-Authored-By: FishingHacks <63118479+FishingHacks@users.noreply.github.com>

--- src/main/kotlin/redcrafter07/processed/block/BlockPipe.kt
+++ src/main/kotlin/redcrafter07/processed/block/BlockPipe.kt
@@ -5,11 +5,13 @@ import net.minecraft.core.Direction
 import net.minecraft.world.item.context.BlockPlaceContext
 import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.EntityBlock
+import net.minecraft.world.level.block.PipeBlock
 import net.minecraft.world.level.block.SoundType
 import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.block.state.BlockState
 import net.minecraft.world.level.block.state.StateDefinition
 import net.minecraft.world.level.block.state.properties.BooleanProperty
+import net.neoforged.neoforge.capabilities.Capabilities
 import redcrafter07.processed.block.tile_entities.PipeBlockEntity
 
 class BlockPipe : Block(Properties.of().sound(SoundType.STONE).isRedstoneConductor { _, _, _ -> false }), EntityBlock {
@@ -34,26 +36,30 @@ class BlockPipe : Block(Properties.of().sound(SoundType.STONE).isRedstoneConduct
 
     override fun getStateForPlacement(context: BlockPlaceContext): BlockState? {
         val block_pos = context.clickedPos
-        val level = context.level;
-        val default_block_state = defaultBlockState();
+        val level = context.level
+        val default_block_state = defaultBlockState()
 
         for (direction in Direction.stream()) {
-            val blockState = context.level.getBlockState(block_pos.relative(direction));
-            var is_connected = false;
-            if (blockState != null && blockState is P)
+            val blockState = context.level.getBlockState(block_pos.relative(direction))
+            var is_connected = false
+            if (blockState.block is PipeBlock) {
+                is_connected = true
+            } else {
+                is_connected = (level.getCapability(
+                    Capabilities.ItemHandler.BLOCK,
+                    block_pos.relative(direction),
+                    direction.opposite
+                ) != null)
+            }
+            default_block_state.setValue(propertyForDirection(direction), is_connected)
         }
 
-        return default_block_state;
+        return default_block_state
     }
 
     override fun createBlockStateDefinition(stateDefinition: StateDefinition.Builder<Block, BlockState>) {
         stateDefinition.add(
-            PIPE_STATE_UP,
-            PIPE_STATE_DOWN,
-            PIPE_STATE_NORTH,
-            PIPE_STATE_EAST,
-            PIPE_STATE_SOUTH,
-            PIPE_STATE_WEST
+            PIPE_STATE_UP, PIPE_STATE_DOWN, PIPE_STATE_NORTH, PIPE_STATE_EAST, PIPE_STATE_SOUTH, PIPE_STATE_WEST
         )
     }
 
