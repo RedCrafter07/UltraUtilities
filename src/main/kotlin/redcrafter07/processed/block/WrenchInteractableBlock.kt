7f039db1ada10082eb7afe72d540b2235340d54d
RedCrafter07 <gh@r07.dev>
2024-04-05 17:55:59 +0200

    Remove shift & add onWrenchInfo

--- src/main/kotlin/redcrafter07/processed/block/WrenchInteractableBlock.kt
+++ src/main/kotlin/redcrafter07/processed/block/WrenchInteractableBlock.kt
@@ -1,8 +1,11 @@
 package redcrafter07.processed.block
 
+import net.minecraft.world.entity.player.Player
 import net.minecraft.world.item.context.UseOnContext
 import net.minecraft.world.level.block.state.BlockState
 
 interface WrenchInteractableBlock {
-    fun onWrenchUse(context: UseOnContext, state: BlockState, withShift: Boolean) {}
+    fun onWrenchUse(context: UseOnContext, state: BlockState) {}
+
+    fun onWrenchInfo(player: Player, state: BlockState) {}
 }
