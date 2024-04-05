9f7401a126948005a94c3f0a1597565ebe28b121
RedCrafter07 <gh@r07.dev>
2024-04-05 17:31:30 +0200

    Add shift boolean to function

--- src/main/kotlin/redcrafter07/processed/block/WrenchInteractableBlock.kt
+++ src/main/kotlin/redcrafter07/processed/block/WrenchInteractableBlock.kt
@@ -4,5 +4,5 @@ import net.minecraft.world.item.context.UseOnContext
 import net.minecraft.world.level.block.state.BlockState
 
 interface WrenchInteractableBlock {
-    fun onWrenchUse(context: UseOnContext, state: BlockState) {}
+    fun onWrenchUse(context: UseOnContext, state: BlockState, withShift: Boolean) {}
 }
