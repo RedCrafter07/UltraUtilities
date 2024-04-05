c21494c8d78ee1b8611c6f6daee68c2fa3d5289c
RedCrafter07 <gh@r07.dev>
2024-04-05 17:31:36 +0200

    Add shifting logic

--- src/main/kotlin/redcrafter07/processed/item/WrenchItem.kt
+++ src/main/kotlin/redcrafter07/processed/item/WrenchItem.kt
@@ -5,6 +5,7 @@ import net.minecraft.world.InteractionResult
 import net.minecraft.world.item.Item
 import net.minecraft.world.item.ItemStack
 import net.minecraft.world.item.context.UseOnContext
+import redcrafter07.processed.ProcessedMod
 import redcrafter07.processed.block.WrenchInteractableBlock
 
 class WrenchItem : Item(Properties().stacksTo(1)) {
@@ -12,9 +13,12 @@ class WrenchItem : Item(Properties().stacksTo(1)) {
     override fun onItemUseFirst(stack: ItemStack, context: UseOnContext): InteractionResult {
         val blockState = context.level.getBlockState(context.clickedPos) ?: return super.onItemUseFirst(stack, context);
         val block = blockState.block;
+        val withShift = context.player?.isShiftKeyDown ?: false;
+
+        ProcessedMod.LOGGER.info("Wrench used on block: $block with shift: $withShift");
 
         if (block is WrenchInteractableBlock) {
-            block.onWrenchUse(context, blockState);
+            block.onWrenchUse(context, blockState, withShift);
             return InteractionResult.SUCCESS;
         } else {
             return super.onItemUseFirst(stack, context);
