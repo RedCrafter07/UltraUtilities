193e1fef65fcbd3543166a934f4e633d3a060f29
RedCrafter07 <gh@r07.dev>
2024-04-05 21:59:37 +0200

    Refactor

--- src/main/kotlin/redcrafter07/processed/item/ModItem.kt
+++ src/main/kotlin/redcrafter07/processed/item/ModItem.kt
@@ -7,9 +7,7 @@ import net.minecraft.world.item.ItemStack
 import net.minecraft.world.item.TooltipFlag
 import net.minecraft.world.level.Level
 
-open class ModItem(properties: Properties, itemID: String) : Item(properties) {
-    private val itemID: String = itemID
-
+open class ModItem(properties: Properties, private val itemID: String) : Item(properties) {
     override fun appendHoverText(stack: ItemStack, world: Level?, tooltip: MutableList<Component>, flag: TooltipFlag) {
         if (Screen.hasShiftDown()) {
             tooltip.add(Component.translatable("item.$itemID.tooltip"))
