70d4f272de929bc52f003b5a5c7017c6453785e2
RedCrafter07 <gh@r07.dev>
2024-04-05 15:42:15 +0200

    Add & fix state logic
    Co-Authored-By: FishingHacks <63118479+FishingHacks@users.noreply.github.com>

--- src/main/kotlin/redcrafter07/processed/block/BlockPipe.kt
+++ src/main/kotlin/redcrafter07/processed/block/BlockPipe.kt
@@ -6,6 +6,7 @@ import net.minecraft.world.item.context.UseOnContext
 import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.SoundType
 import net.minecraft.world.level.block.state.BlockState
+import net.minecraft.world.level.block.state.StateDefinition
 import net.minecraft.world.level.block.state.properties.EnumProperty
 
 enum class PipeLikeState(name: String) : StringRepresentable {
@@ -29,6 +30,18 @@ enum class PipeLikeState(name: String) : StringRepresentable {
             return Normal;
         }
     }
+
+    fun translation(): Component {
+        return when(this) {
+            //wait
+            Normal -> Component.translatable("processed.pipe_like_state.normal")
+            Pull -> Component.translatable("processed.pipe_like_state.pull")
+            Push -> Component.translatable("processed.pipe_like_state.push")
+            else -> Component.literal("Err: Unreachable pipe state")
+        }
+
+        //where does the state translation go to? Like, where do you see it?
+    }
 }
 
 class BlockPipe() : Block(
@@ -39,14 +52,22 @@ class BlockPipe() : Block(
     private val PIPE_STATE = EnumProperty.create("state", PipeLikeState::class.java);
 
     init {
-//        val defaultState = this.defaultBlockState()
-//        defaultState.setValue(PIPE_STATE, PipeLikeState.Normal)
+        val defaultState = this.defaultBlockState()
+        defaultState.setValue(PIPE_STATE, PipeLikeState.Normal)
+    }
+
+    override fun createBlockStateDefinition(pBuilder: StateDefinition.Builder<Block, BlockState>) {
+        pBuilder.add(PIPE_STATE);
+        super.createBlockStateDefinition(pBuilder);
     }
 
     override fun onWrenchUse(context: UseOnContext, state: BlockState) {
         val oldPipeState = state.getValue(PIPE_STATE);
         val newPipeState = oldPipeState.next();
         state.setValue(PIPE_STATE, newPipeState);
-        context.player?.sendSystemMessage(Component.literal("Pipestate: " + oldPipeState.name + " -> " + newPipeState.name));
+        context.player?.sendSystemMessage(
+            Component.translatable("processed.pipe_like_state.update",
+                oldPipeState.translation(),
+                newPipeState.translation()));
     }
 }
