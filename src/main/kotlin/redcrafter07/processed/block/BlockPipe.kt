adb103bb0626c7b17c8b367b46625c8c00585656
RedCrafter07 <gh@r07.dev>
2024-04-05 20:39:15 +0200

    Pipe block VIP

--- src/main/kotlin/redcrafter07/processed/block/BlockPipe.kt
+++ src/main/kotlin/redcrafter07/processed/block/BlockPipe.kt
@@ -1,117 +1,50 @@
 package redcrafter07.processed.block
 
-import net.minecraft.client.resources.sounds.Sound
-import net.minecraft.network.chat.Component
-import net.minecraft.network.protocol.game.ClientboundSetActionBarTextPacket
-import net.minecraft.server.level.ServerPlayer
-import net.minecraft.sounds.SoundEvents
-import net.minecraft.util.StringRepresentable
-import net.minecraft.world.entity.player.Player
-import net.minecraft.world.item.context.BlockPlaceContext
-import net.minecraft.world.item.context.UseOnContext
+import net.minecraft.core.BlockPos
 import net.minecraft.world.level.block.Block
+import net.minecraft.world.level.block.EntityBlock
 import net.minecraft.world.level.block.SoundType
+import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.block.state.BlockState
 import net.minecraft.world.level.block.state.StateDefinition
-import net.minecraft.world.level.block.state.properties.EnumProperty
-import net.minecraft.world.level.block.state.properties.NoteBlockInstrument
-import net.neoforged.neoforge.client.event.sound.SoundEvent
+import net.minecraft.world.level.block.state.properties.BooleanProperty
+import redcrafter07.processed.block.tile_entities.PipeBlockEntity
 
-enum class PipeLikeState : StringRepresentable {
-    None, Push, Pull;
-
-    override fun getSerializedName(): String {
-        return when (this) {
-            None -> "none"
-            Pull -> "pull"
-            Push -> "push"
-        }
-    }
-
-    fun next(): PipeLikeState {
-        return when (this) {
-            None -> Pull
-            Pull -> Push
-            Push -> None
-        }
-    }
-
-    fun translation(withColor: Boolean = false): Component {
-        val base = "processed.pipe_state"
-        val addition = when (withColor) {
-            true -> "title."
-            false -> ""
-        }
-        val state = when (this) {
-            None -> "none"
-            Pull -> "pull"
-            Push -> "push"
-        }
-
-        val formattedString = Component.translatable("${base}.${addition}${state}")
-
-        return formattedString
-    }
-}
-
-class BlockPipe : Block(Properties.of().sound(SoundType.STONE).isRedstoneConductor { _, _, _ -> false }),
-    WrenchInteractableBlock {
+class BlockPipe : Block(Properties.of().sound(SoundType.STONE).isRedstoneConductor { _, _, _ -> false }), EntityBlock {
     companion object {
-        private val pipeState = EnumProperty.create("pipe_state", PipeLikeState::class.java)
+        val PIPE_STATE_TOP = BooleanProperty.create("PipeConnectionTop")
+        val PIPE_STATE_BOTTOM = BooleanProperty.create("PipeConnectionBottom")
+        val PIPE_STATE_NORTH = BooleanProperty.create("PipeConnectionNorth")
+        val PIPE_STATE_SOUTH = BooleanProperty.create("PipeConnectionSouth")
+        val PIPE_STATE_WEST = BooleanProperty.create("PipeConnectionWest")
+        val PIPE_STATE_EAST = BooleanProperty.create("PipeConnectionEast")
     }
 
-    init {
-        registerDefaultState(stateDefinition.any().setValue(pipeState, PipeLikeState.None))
+//    override fun getStateForPlacement(context: BlockPlaceContext): BlockState? {
+//        val block_pos = context.clickedPos
+//        val level = context.level;
+//        val below = block_pos.below()
+//        val north = block_pos.north()
+//        val south = block_pos.south()
+//        val east = block_pos.east()
+//        val west = block_pos.west()
+//        val default_block_state = defaultBlockState();
+//
+//        return null;
+//    }
+
+    override fun createBlockStateDefinition(stateDefinition: StateDefinition.Builder<Block, BlockState>) {
+        stateDefinition.add(
+            PIPE_STATE_TOP,
+            PIPE_STATE_BOTTOM,
+            PIPE_STATE_NORTH,
+            PIPE_STATE_EAST,
+            PIPE_STATE_SOUTH,
+            PIPE_STATE_WEST
+        )
     }
 
-    override fun createBlockStateDefinition(pBuilder: StateDefinition.Builder<Block, BlockState>) {
-        pBuilder.add(pipeState)
-    }
-
-    override fun getStateForPlacement(p_49820_: BlockPlaceContext): BlockState? {
-        return super.getStateForPlacement(p_49820_)
-    }
-
-    override fun onWrenchInfo(player: Player, state: BlockState) {
-        val pipeState = state.getValue(pipeState)
-
-        //ui.button.click
-        player.playSound(SoundEvents.UI_BUTTON_CLICK.value(), .25f, 1.0f)
-
-        if (player is ServerPlayer) {
-            player.connection.send(
-                ClientboundSetActionBarTextPacket(
-                    Component.translatable(
-                        "processed.pipe_state", pipeState.translation(true)
-                    )
-                )
-            )
-        }
-    }
-
-    override fun onWrenchUse(context: UseOnContext, state: BlockState) {
-        val newPipeState = state.getValue(pipeState).next()
-        val newState = state.setValue(pipeState, newPipeState)
-        context.level.setBlock(context.clickedPos, newState, UPDATE_CLIENTS or UPDATE_NEIGHBORS)
-
-        val player = context.player
-
-        val pitch = when (newPipeState) {
-            PipeLikeState.Pull -> 1.0f
-            PipeLikeState.Push -> 1.2f
-            PipeLikeState.None -> 1.5f
-        }
-
-        player?.playSound(NoteBlockInstrument.BELL.soundEvent.value(), 1.0f, pitch)
-
-        if (player is ServerPlayer) {
-            player.connection.send(
-                ClientboundSetActionBarTextPacket(
-                    Component.translatable(
-                        "processed.pipe_state", newPipeState.translation(true)
-                    )
-                )
-            )
-        }
+    override fun newBlockEntity(pos: BlockPos, state: BlockState): BlockEntity {
+        return PipeBlockEntity(pos, state)
     }
 }
