a60543583168d95c24a8ab04e0455a7f9bec3444
RedCrafter07 <gh@r07.dev>
2024-04-05 15:35:48 +0200

    Update import
    Co-Authored-By: FishingHacks <63118479+FishingHacks@users.noreply.github.com>

--- src/main/kotlin/redcrafter07/processed/datagen/ModLootTableProvider.kt
+++ src/main/kotlin/redcrafter07/processed/datagen/ModLootTableProvider.kt
@@ -3,12 +3,12 @@ package redcrafter07.processed.datagen
 import net.minecraft.data.PackOutput
 import net.minecraft.data.loot.LootTableProvider
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets
-import redcrafter07.processed.datagen.loot.ModBlogLootTables
+import redcrafter07.processed.datagen.loot.ModBlockLootTables
 
 object ModLootTableProvider {
     fun create(output: PackOutput): LootTableProvider {
         return LootTableProvider(output, setOf(), listOf(
-            LootTableProvider.SubProviderEntry(::ModBlogLootTables, LootContextParamSets.BLOCK)
+            LootTableProvider.SubProviderEntry(::ModBlockLootTables, LootContextParamSets.BLOCK)
         ));
     }
 }
