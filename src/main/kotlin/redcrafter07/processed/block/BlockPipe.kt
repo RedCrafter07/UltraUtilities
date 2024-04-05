b3e87603de2b8326d04dde9ca702f335aac9f0fd
RedCrafter07 <gh@r07.dev>
2024-04-05 16:27:51 +0200

    Fixes & more
    Needs some bugfixing anyways though
    
    Co-Authored-By: FishingHacks <63118479+FishingHacks@users.noreply.github.com>

--- src/main/kotlin/redcrafter07/processed/block/BlockPipe.kt
+++ src/main/kotlin/redcrafter07/processed/block/BlockPipe.kt
@@ -2,72 +2,75 @@ package redcrafter07.processed.block
 
 import net.minecraft.network.chat.Component
 import net.minecraft.util.StringRepresentable
+import net.minecraft.world.item.context.BlockPlaceContext
 import net.minecraft.world.item.context.UseOnContext
 import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.SoundType
 import net.minecraft.world.level.block.state.BlockState
 import net.minecraft.world.level.block.state.StateDefinition
 import net.minecraft.world.level.block.state.properties.EnumProperty
+import redcrafter07.processed.ProcessedMod
 
-enum class PipeLikeState(name: String) : StringRepresentable {
-    Normal("Normal"),
-    Push("Push"),
-    Pull("Pull");
+enum class PipeLikeState : StringRepresentable {
+    None,
+    Push,
+    Pull;
 
     override fun getSerializedName(): String {
-        return this.name;
+        return when (this) {
+            None -> "none"
+            Pull -> "pull"
+            Push -> "push"
+        }
     }
 
     fun next(): PipeLikeState {
-        if (this == Normal) {
-            return Push;
-        } else if (this == Push) {
-            return Pull;
-        } else if (this == Pull) {
-            return Normal;
-        } else {
-            // should be unreachable
-            return Normal;
+        return when (this) {
+            None -> Pull
+            Pull -> Push
+            Push -> None
         }
     }
 
     fun translation(): Component {
-        return when(this) {
-            //wait
-            Normal -> Component.translatable("processed.pipe_like_state.normal")
+        return when (this) {
+            None -> Component.translatable("processed.pipe_like_state.none")
             Pull -> Component.translatable("processed.pipe_like_state.pull")
             Push -> Component.translatable("processed.pipe_like_state.push")
-            else -> Component.literal("Err: Unreachable pipe state")
         }
-
-        //where does the state translation go to? Like, where do you see it?
     }
 }
 
-class BlockPipe() : Block(
-    Properties.of()
-        .sound(SoundType.STONE)
-        .isRedstoneConductor(StatePredicate { _, _, _ -> false })
+class BlockPipe : Block(
+    Properties.of().sound(SoundType.STONE).isRedstoneConductor { _, _, _ -> false }
 ), WrenchInteractableBlock {
-    private val PIPE_STATE = EnumProperty.create("state", PipeLikeState::class.java);
+    companion object {
+        private val pipeState = EnumProperty.create("pipe_state", PipeLikeState::class.java)
+    }
 
     init {
-        val defaultState = this.defaultBlockState()
-        defaultState.setValue(PIPE_STATE, PipeLikeState.Normal)
+        registerDefaultState(stateDefinition.any().setValue(pipeState, PipeLikeState.None))
     }
 
     override fun createBlockStateDefinition(pBuilder: StateDefinition.Builder<Block, BlockState>) {
-        pBuilder.add(PIPE_STATE);
-        super.createBlockStateDefinition(pBuilder);
+        pBuilder.add(pipeState)
+    }
+
+    override fun getStateForPlacement(p_49820_: BlockPlaceContext): BlockState? {
+        return super.getStateForPlacement(p_49820_)
     }
 
     override fun onWrenchUse(context: UseOnContext, state: BlockState) {
-        val oldPipeState = state.getValue(PIPE_STATE);
-        val newPipeState = oldPipeState.next();
-        state.setValue(PIPE_STATE, newPipeState);
+        val oldPipeState = state.getValue(pipeState)
+        val newPipeState = oldPipeState.next()
+        state.setValue(pipeState, newPipeState)
+        context.level.setBlock(context.clickedPos, state, UPDATE_CLIENTS or UPDATE_NEIGHBORS)
+        ProcessedMod.LOGGER.info("Pos: " + context.clickedPos)
+
         context.player?.sendSystemMessage(
-            Component.translatable("processed.pipe_like_state.update",
-                oldPipeState.translation(),
-                newPipeState.translation()));
+            Component.translatable(
+                "processed.pipe_like_state.update", oldPipeState.translation(), newPipeState.translation()
+            )
+        )
     }
 }
