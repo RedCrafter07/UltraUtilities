bcddcebf241030621650ba5e94a0cb6f3e15f56a
FishingHacks <fishinghacks@proton.me>
2024-04-06 21:05:16 +0200

    added pipe pressurizer scanning

--- src/main/kotlin/redcrafter07/processed/block/tile_entities/PipeBlockEntity.kt
+++ src/main/kotlin/redcrafter07/processed/block/tile_entities/PipeBlockEntity.kt
@@ -6,16 +6,28 @@ import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.block.state.BlockState
 import redcrafter07.processed.block.DirectionalPipeLikeState
 
-class PipeBlockEntity(pos: BlockPos, state: BlockState): BlockEntity(ModTileEntities.PIPE_BLOCK_ENTITY.get(), pos, state) {
+class PipeBlockEntity(pos: BlockPos, state: BlockState) :
+    BlockEntity(ModTileEntities.PIPE_BLOCK_ENTITY.get(), pos, state) {
     val pipeState = DirectionalPipeLikeState()
+    var pipePresurizerPos: BlockPos? = null
 
     override fun saveAdditional(nbt: CompoundTag) {
         super.saveAdditional(nbt)
         pipeState.saveToNBT("state", nbt)
+        val pipePresurizerPos = pipePresurizerPos
+        if (pipePresurizerPos == null) {
+            nbt.putIntArray("pipePressurizerPos", listOf())
+        } else {
+            nbt.putIntArray("pipePressurizerPos", listOf(pipePresurizerPos.x, pipePresurizerPos.y, pipePresurizerPos.z))
+        }
     }
 
     override fun load(nbt: CompoundTag) {
         super.load(nbt)
         pipeState.loadFromNBT("state", nbt)
+        val arr = nbt.getIntArray("pipePressurizerPos")
+        if (arr.count() == 3) {
+            pipePresurizerPos = BlockPos(arr[0], arr[1], arr[2])
+        }
     }
 }
--- src/main/kotlin/redcrafter07/processed/block/tile_entities/PipePressurizerBlockEntity.kt
+++ src/main/kotlin/redcrafter07/processed/block/tile_entities/PipePressurizerBlockEntity.kt
@@ -1,16 +1,49 @@
 package redcrafter07.processed.block.tile_entities
 
 import net.minecraft.core.BlockPos
+import net.minecraft.core.Direction
 import net.minecraft.nbt.CompoundTag
+import net.minecraft.world.level.LevelAccessor
 import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.block.state.BlockState
-import redcrafter07.processed.block.DirectionalPipeLikeState
+import redcrafter07.processed.block.PipeLikeState
+import java.util.Stack
 
-class PipePressurizerBlockEntity(pos: BlockPos, state: BlockState): BlockEntity(ModTileEntities.PIPE_BLOCK_ENTITY.get(), pos, state) {
-    val connectedPipes: List<BlockPos> = listOf()
+class PipePressurizerBlockEntity(pos: BlockPos, state: BlockState) :
+    BlockEntity(ModTileEntities.PIPE_BLOCK_ENTITY.get(), pos, state) {
+    val connectedPipes: Stack<BlockPos> = Stack()
+
+    private class DirectionalPosition(val blockPos: BlockPos, val direction: Direction) {}
+
+    fun scanNetwork(level: LevelAccessor, myBlockPos: BlockPos) {
+        connectedPipes.clear()
+        val blocksToScan: Stack<DirectionalPosition> = Stack();
+        blocksToScan.push(DirectionalPosition(myBlockPos.relative(Direction.NORTH), Direction.NORTH))
+        blocksToScan.push(DirectionalPosition(myBlockPos.relative(Direction.SOUTH), Direction.SOUTH))
+        blocksToScan.push(DirectionalPosition(myBlockPos.relative(Direction.WEST), Direction.WEST))
+        blocksToScan.push(DirectionalPosition(myBlockPos.relative(Direction.EAST), Direction.EAST))
+
+        while (!blocksToScan.isEmpty()) {
+            val directionalPosition = blocksToScan.pop() ?: continue
+            if (connectedPipes.contains(directionalPosition.blockPos)) continue
+            val blockEntity = level.getBlockEntity(directionalPosition.blockPos) ?: continue
+            if (blockEntity !is PipeBlockEntity) continue
+            if (blockEntity.pipeState.getState(directionalPosition.direction.opposite) == PipeLikeState.None) continue
+
+            blockEntity.pipePresurizerPos = myBlockPos
+            connectedPipes.push(directionalPosition.blockPos);
+            for (direction in Direction.stream()) {
+                val newBlockPos = DirectionalPosition(directionalPosition.blockPos.relative(direction), direction)
+                if (connectedPipes.contains(newBlockPos.blockPos)) continue
+                if (blocksToScan.contains(newBlockPos)) continue
+                blocksToScan.push(newBlockPos)
+            }
+        }
+    }
 
     override fun saveAdditional(nbt: CompoundTag) {
         super.saveAdditional(nbt)
+
     }
 
     override fun load(nbt: CompoundTag) {
