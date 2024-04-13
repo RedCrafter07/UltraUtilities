c733c575c5a0f50967aff4beec8b25af9e82fd12
RedCrafter07 <gh@r07.dev>
2024-04-13 14:53:40 +0200

    Add wrenchmode enum class

---
+++ src/main/kotlin/redcrafter07/processed/item/WrenchMode.kt
@@ -0,0 +1,42 @@
+package redcrafter07.processed.item
+
+enum class WrenchMode {
+    Config, Upgrade, Rotate;
+
+    fun next(): WrenchMode {
+        return when (this) {
+            Config -> Upgrade
+            Upgrade -> Rotate
+            Rotate -> Config
+        }
+    }
+
+    fun translation(): String {
+        val prefix = "item.processed.wrench.mode"
+        val suffix = when (this) {
+            Config -> "config"
+            Upgrade -> "upgrade"
+            Rotate -> "rotate"
+        }
+
+        return "$prefix.$suffix"
+    }
+
+    fun save(): UShort {
+        return when (this) {
+            Config -> 0.toUShort()
+            Upgrade -> 1.toUShort()
+            Rotate -> 2.toUShort()
+            else -> 0.toUShort()
+        }
+    }
+
+    fun load(value: UShort): WrenchMode {
+        return when (value.toInt()) {
+            0 -> Config
+            1 -> Upgrade
+            2 -> Rotate
+            else -> Config
+        }
+    }
+}
