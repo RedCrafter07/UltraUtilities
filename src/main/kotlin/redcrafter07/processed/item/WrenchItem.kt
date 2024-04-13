a9dadb71a3137269076f8551daacf6d326ec504c
RedCrafter07 <gh@r07.dev>
2024-04-13 14:53:50 +0200

    Implement mode change

--- src/main/kotlin/redcrafter07/processed/item/WrenchItem.kt
+++ src/main/kotlin/redcrafter07/processed/item/WrenchItem.kt
@@ -1,15 +1,32 @@
 package redcrafter07.processed.item
 
+import net.minecraft.network.chat.Component
+import net.minecraft.network.chat.MutableComponent
+import net.minecraft.world.InteractionHand
 import net.minecraft.world.InteractionResult
+import net.minecraft.world.InteractionResultHolder
+import net.minecraft.world.entity.player.Player
 import net.minecraft.world.item.ItemStack
 import net.minecraft.world.item.context.UseOnContext
+import net.minecraft.world.level.Level
 import redcrafter07.processed.block.WrenchInteractableBlock
 
-
 class WrenchItem : ModItem(Properties().stacksTo(1), "wrench") {
+    private fun getMode(stack: ItemStack): WrenchMode {
+        val nbt = stack.orCreateTag
+        return WrenchMode.Config.load(nbt.getShort("mode").toUShort())
+    }
+
+    override fun getDefaultInstance(): ItemStack {
+        val stack = super.getDefaultInstance()
+        val nbt = stack.orCreateTag
+
+        nbt.putShort("mode", WrenchMode.Config.save().toShort())
+
+        return stack
+    }
 
     override fun onItemUseFirst(stack: ItemStack, context: UseOnContext): InteractionResult {
-//        if (context.level.isClientSide) return super.onItemUseFirst(stack, context)
         val blockState = context.level.getBlockState(context.clickedPos) ?: return super.onItemUseFirst(stack, context)
         val block = blockState.block
 
@@ -24,5 +41,22 @@ class WrenchItem : ModItem(Properties().stacksTo(1), "wrench") {
             return InteractionResult.SUCCESS
         }
         return super.onItemUseFirst(stack, context)
+    }
+
+    override fun use(level: Level, player: Player, hand: InteractionHand): InteractionResultHolder<ItemStack> {
+        val stack = player.getItemInHand(hand)
+        val mode = getMode(stack).next()
+        val nbt = stack.orCreateTag
+        nbt.putShort("mode", mode.save().toShort())
+
+        return InteractionResultHolder.success(stack)
+    }
+
+    override fun getAdditionalTooltip(stack: ItemStack, world: Level?): MutableComponent {
+        return Component.translatable("item.processed.wrench.mode", Component.translatable(getMode(stack).translation()))
+    }
+
+    override fun getName(stack: ItemStack): Component {
+        return Component.translatable("item.processed.wrench.name", Component.translatable(getMode(stack).translation()))
     }
 }
